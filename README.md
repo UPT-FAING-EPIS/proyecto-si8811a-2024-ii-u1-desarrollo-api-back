dae103997099fdac5652c966a5c077223f2e244a
HelbertCLinUPT <hc2020067571@virtual.upt.pe>
2024-08-24 17:08:43 -0500

    terraform v3 issue #2

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/vK6WBQ1t)
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=15560937)
+
+###Este archivo main.tf incluye:
+Configuración del proveedor Azure.
+Un grupo de recursos.
+Dos APIs como contenedores con sus respectivos App Services y Container Registry.
+Dos bases de datos MongoDB en Cosmos DB.
+Un frontend React como contenedor con su propio App Service y Container Registry.
+Outputs para las URLs de las APIs y el frontend, así como las cadenas de conexión de las bases de datos.
+
+###Los placeholders que necesitas reemplazar son:
+#{RESOURCE_GROUP_NAME}
+#{AZURE_REGION}
+#{API_ACR_NAME}
+#{API_APP_SERVICE_PLAN_NAME}
+#{API1_APP_NAME}
+#{API1_DOCKER_IMAGE}
+#{API1_DOCKER_IMAGE_TAG}
+#{API2_APP_NAME}
+#{API2_DOCKER_IMAGE}
+#{API2_DOCKER_IMAGE_TAG}
+#{COSMOSDB_ACCOUNT_NAME_1}
+#{MONGODB_NAME_1}
+#{COSMOSDB_ACCOUNT_NAME_2}
+#{MONGODB_NAME_2}
+#{FRONTEND_ACR_NAME}
+#{FRONTEND_APP_SERVICE_PLAN_NAME}
+#{FRONTEND_APP_NAME}
+#{FRONTEND_DOCKER_IMAGE}
+#{FRONTEND_DOCKER_IMAGE_TAG}
+Asegúrate de que todos los nombres de recursos sean únicos en Azure, especialmente para los Container Registries, App Services y cuentas de Cosmos DB.
--- main.tf
+++ main.tf
@@ -13,44 +13,79 @@ provider "azurerm" {
 
 # Grupo de recursos
 resource "azurerm_resource_group" "rg" {
-  name     = "api-resources"
-  location = "East US"
+  name     = "#{RESOURCE_GROUP_NAME}"
+  location = "#{AZURE_REGION}"
 }
 
-# Plan de App Service
+# Container Registry para APIs
+resource "azurerm_container_registry" "acr" {
+  name                = "#{API_ACR_NAME}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Plan de App Service para APIs
 resource "azurerm_service_plan" "asp" {
-  name                = "api-appserviceplan"
+  name                = "#{API_APP_SERVICE_PLAN_NAME}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "F1"  # Plan gratuito
+  sku_name            = "B1"
 }
 
-# App Service para la API
-resource "azurerm_linux_web_app" "api" {
-  name                = "my-netcore-api-CL"  # Asegúrate de que este nombre sea único
+# Primera API como contenedor
+resource "azurerm_linux_web_app" "api1" {
+  name                = "#{API1_APP_NAME}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
+    always_on = true
     application_stack {
-      dotnet_version = "6.0"
+      docker_image     = "#{API1_DOCKER_IMAGE}"
+      docker_image_tag = "#{API1_DOCKER_IMAGE_TAG}"
     }
-    always_on = false
   }
 
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "ASPNETCORE_ENVIRONMENT"              = "Production"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://#{API_ACR_NAME}.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
     "MONGODB_CONNECTION_STRING"           = azurerm_cosmosdb_account.db.primary_key
-    "NEW_API_FEATURE"                     = "enabled"  # Nuevo campo de API
+  }
+}
+
+# Segunda API como contenedor
+resource "azurerm_linux_web_app" "api2" {
+  name                = "#{API2_APP_NAME}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = true
+    application_stack {
+      docker_image     = "#{API2_DOCKER_IMAGE}"
+      docker_image_tag = "#{API2_DOCKER_IMAGE_TAG}"
+    }
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://#{API_ACR_NAME}.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
+    "MONGODB_CONNECTION_STRING"           = azurerm_cosmosdb_account.db2.primary_key
   }
 }
 
 # Primera cuenta de Cosmos DB
 resource "azurerm_cosmosdb_account" "db" {
-  name                = "my-cosmosdb-account-cl-unique"  # Debe ser globalmente único
+  name                = "#{COSMOSDB_ACCOUNT_NAME_1}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
@@ -74,27 +109,14 @@ resource "azurerm_cosmosdb_account" "db" {
 
 # Primera base de datos MongoDB
 resource "azurerm_cosmosdb_mongo_database" "mongodb" {
-  name                = "myDatabase"
+  name                = "#{MONGODB_NAME_1}"
   resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.db.name
-}
-
-# Primera colección MongoDB
-resource "azurerm_cosmosdb_mongo_collection" "collection" {
-  name                = "myCollection"
-  resource_group_name = azurerm_resource_group.rg.name
-  account_name        = azurerm_cosmosdb_account.db.name
-  database_name       = azurerm_cosmosdb_mongo_database.mongodb.name
-
-  index {
-    keys   = ["_id"]
-    unique = true
-  }
 }
 
 # Segunda cuenta de Cosmos DB
 resource "azurerm_cosmosdb_account" "db2" {
-  name                = "my-cosmosdb-account-cl-unique-2"  # Debe ser globalmente único
+  name                = "#{COSMOSDB_ACCOUNT_NAME_2}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
@@ -118,22 +140,73 @@ resource "azurerm_cosmosdb_account" "db2" {
 
 # Segunda base de datos MongoDB
 resource "azurerm_cosmosdb_mongo_database" "mongodb2" {
-  name                = "mySecondDatabase"
+  name                = "#{MONGODB_NAME_2}"
   resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.db2.name
 }
 
-# Outputs
-output "webapp_url" {
-  value = azurerm_linux_web_app.api.default_hostname
+# Azure Container Registry para el frontend
+resource "azurerm_container_registry" "acr_frontend" {
+  name                = "#{FRONTEND_ACR_NAME}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
-output "cosmosdb_connection_string" {
+# Plan de App Service para el frontend
+resource "azurerm_service_plan" "asp_frontend" {
+  name                = "#{FRONTEND_APP_SERVICE_PLAN_NAME}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# App Service para el frontend React
+resource "azurerm_linux_web_app" "frontend" {
+  name                = "#{FRONTEND_APP_NAME}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp_frontend.id
+
+  site_config {
+    always_on = true
+    application_stack {
+      docker_image     = "#{FRONTEND_DOCKER_IMAGE}"
+      docker_image_tag = "#{FRONTEND_DOCKER_IMAGE_TAG}"
+    }
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://#{FRONTEND_ACR_NAME}.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr_frontend.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr_frontend.admin_password
+    "REACT_APP_API1_URL"                  = "https://${azurerm_linux_web_app.api1.default_hostname}"
+    "REACT_APP_API2_URL"                  = "https://${azurerm_linux_web_app.api2.default_hostname}"
+  }
+}
+
+# Outputs
+output "api1_url" {
+  value = "https://${azurerm_linux_web_app.api1.default_hostname}"
+}
+
+output "api2_url" {
+  value = "https://${azurerm_linux_web_app.api2.default_hostname}"
+}
+
+output "frontend_url" {
+  value = "https://${azurerm_linux_web_app.frontend.default_hostname}"
+}
+
+output "cosmosdb_connection_string1" {
   value     = azurerm_cosmosdb_account.db.primary_key
   sensitive = true
 }
 
-output "cosmosdb2_connection_string" {
+output "cosmosdb_connection_string2" {
   value     = azurerm_cosmosdb_account.db2.primary_key
   sensitive = true
 }
